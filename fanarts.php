16e9d62c44f9cdc3d44808269e2ab6d5f8964951
Karn Kamolnavin <596976@egat.co.th>
2023-09-13 16:38:15 +0700

    Test New Fanart JSON

---
+++ fanarts.php
@@ -0,0 +1,13 @@
+<?php
+$url = 'https://raw.githubusercontent.com/lucidkarn/lucian_solutions/main/fanarts.json';
+$json = file_get_contents($url);
+$contributors = json_decode($json);
+
+foreach ($contributors as $entry) {
+    if (array_key_exists('slug', $entry)){
+        echo '<a href="#{strtolower($entry->artist)}" class="btn btn-sm btn-light my-1">{$entry->artist} <small lang="{$entry->slug_lang}">{$entry->slug}</small></a>'
+    }
+    else {
+        echo '<a href="#{strtolower($entry->artist)}" class="btn btn-sm btn-light my-1">{$entry->artist}</a>';
+    }
+}
