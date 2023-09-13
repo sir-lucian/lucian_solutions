175407687490470fbc746b43ad025563d28df800
Karn Kamolnavin <596976@egat.co.th>
2023-09-13 16:50:40 +0700

    Fixing JSON Decode

--- fanarts.php
+++ fanarts.php
@@ -3,11 +3,11 @@ $url = 'https://raw.githubusercontent.com/lucidkarn/lucian_solutions/main/fanart
 $json = file_get_contents($url);
 $contributors = json_decode($json);
 
-foreach ($contributors as $entry) {
+foreach ($contributors as $entry) {/*
     if (array_key_exists('slug', $entry)){
-        echo '<a href="#{strtolower($entry->artist)}" class="btn btn-sm btn-light my-1">{$entry->artist} <small lang="{$entry->slug_lang}">{$entry->slug}</small></a>'
+        echo '<a href="#'.strtolower($entry->artist).'" class="btn btn-sm btn-light my-1">'.$entry->artist.' <small lang="'.$entry->slug_lang.'">'.$entry->slug.'</small></a>';
     }
-    else {
-        echo '<a href="#{strtolower($entry->artist)}" class="btn btn-sm btn-light my-1">{$entry->artist}</a>';
+    else {*/
+        echo '<a href="#'.strtolower($entry->artist).'" class="btn btn-sm btn-light my-1">'.$entry->artist.'</a>';
     }
-}
+?>
