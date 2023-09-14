a6b9648c6530bb713103fe9e1c40f2492de72ceb
Karn Kamolnavin <596976@egat.co.th>
2023-09-14 23:28:37 +0700

    Fix minor visual

--- php/artist.list.php
+++ php/artist.list.php
@@ -5,9 +5,10 @@ $json = file_get_contents($url);
 $contributors = json_decode($json);
 
 foreach ($contributors as $entry) {
+    echo '<a href="#'.preg_replace('/\s+/', '_', strtolower($entry->artist)).'" class="artist-list btn btn-sm btn-light px-2 m-1">';
+    echo '<strong style="color: #d4af37;">#</strong> '.$entry->artist;
     if (isset($entry->slug)) {
-        echo '<a href="#'.preg_replace('/\s+/', '_', strtolower($entry->artist)).'" class="artist-list btn btn-sm btn-light px-2 m-1">'.$entry->artist.' <small lang="'.$entry->slug_lang.'">'.$entry->slug.'</small></a>';
-    } else {
-        echo '<a href="#'.preg_replace('/\s+/', '_', strtolower($entry->artist)).'" class="artist-list btn btn-sm btn-light px-2 m-1">'.$entry->artist.'</a>';
+        echo ' <small lang="'.$entry->slug_lang.'">'.$entry->slug.'</small>';
     }
+    echo '</a>';
 }
