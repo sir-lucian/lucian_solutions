ad9a1396649bb4db3d82b6b47899117ef89905fd
Karn Kamolnavin <596976@egat.co.th>
2023-09-15 11:20:35 +0700

    Minor bug fixes

--- php/artwork.list.php
+++ php/artwork.list.php
@@ -41,7 +41,7 @@ foreach ($contributors as $entry) {
         }
         echo ' class="img-fluid h-100 lucian-showcase square-pix ratio ratio-1-1" loading="lazy" />';
         echo '<div class="position-absolute top-50 start-50 translate-middle w-100 h-100 img-card-gradient"></div>';
-        echo '<div class="bg-dark text-white px-2 py-1 position-absolute bottom-0 end-0 fw-bold rounded-3" style="font-size: 0.8em; translate: -0.5em -0.5em;">'.$entry->artist.'</div>';
+        echo '<div class="bg-dark shadow-sm text-white px-2 py-1 position-absolute bottom-0 end-0 fw-bold rounded-3" style="font-size: 0.8em; translate: -0.5em -0.5em;">'.$entry->artist.'</div>';
         echo '</a>';
         ++$artNumber;
     }
