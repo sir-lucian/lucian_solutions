0d3e2f7e8560d285a21418d467bb55c605bef07b
Karn Kamolnavin <596976@egat.co.th>
2023-11-24 15:32:20 +0700

    จัดส่งไปรษณีย์รอบที่ 1

---
+++ php/postcard-hny2024.php
@@ -0,0 +1,11 @@
+<?php
+
+$url = 'https://raw.githubusercontent.com/lucidkarn/lucian_solutions/main/postcard-hny2024.json';
+$json = file_get_contents($url);
+$postcardList = json_decode($json);
+
+echo '<div class="text-center d-block"><ol class="d-inline-block text-start">';
+foreach ($postcardList as $entry) {
+    echo '<li><p><i class="bi bi-person-fill mx-2 text-' . $entry->color . '"></i><strong>' . $entry->name . '</strong> <small class="fst-italic">' . $entry->method . '</small></p></li>';
+}
+echo '</ol></div>';
