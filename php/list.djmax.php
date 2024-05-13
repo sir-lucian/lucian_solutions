6c4a239cdad192d8de2f42fb5a3bd195ed54d886
Karn Kamolnavin <596976@egat.co.th>
2024-05-13 23:25:00 +0700

    Adjust DJMax's Table

--- php/list.djmax.php
+++ php/list.djmax.php
@@ -3,17 +3,19 @@
 $url = 'https://raw.githubusercontent.com/lucidkarn/lucian_solutions/main/json/list.djmax.json';
 $json = file_get_contents($url);
 $data = json_decode($json);
-$blank_cell = '<td class="text-success text-center"></td>';
-$checked_cell = ['<td class="', ' text-center">&check;</td>'];
+$blank_cell = '<td class="border-0 fw-bold"></td>';
+$checked_cell = ['<td class="', ' border-0"></td>'];
+$new_badge = ' <span class="badge rounded-pill bg-danger">New</span>';
 
 foreach ($data->charts as $chart) {
-    echo '<tr><td>'.$chart->name;
-    echo (isset($chart->local_name) && !empty($chart->local_name)) ? ' <small lang="'.$chart->local_lang.'">'.$chart->local_name.'</small></td>' : '</td>';
-    echo '<td>'.$chart->pack.'</td>';
-    echo (in_array(4, $chart->sc)) ? $checked_cell[0].'text-success'.$checked_cell[1] : $blank_cell;
-    echo (in_array(5, $chart->sc)) ? $checked_cell[0].'text-primary'.$checked_cell[1] : $blank_cell;
-    echo (in_array(6, $chart->sc)) ? $checked_cell[0].'text-warning'.$checked_cell[1] : $blank_cell;
-    echo (in_array(8, $chart->sc)) ? $checked_cell[0].'text-danger'.$checked_cell[1] : $blank_cell;
-    echo (isset($chart->remarks) && !empty($chart->remarks)) ? '<td>'.$chart->remarks.'</td>' : $blank_cell;
+    echo '<tr><td class="border-0 fw-bold">'.$chart->name;
+    echo (isset($chart->local_name) && !empty($chart->local_name)) ? ' <small lang="'.$chart->local_lang.'" class="text-muted fw-light">'.$chart->local_name.'</small>' : '';
+    echo (isset($chart->remarks) && $chart->remarks == 'Season 12') ? $new_badge : '';
+    echo '</td><td class="border-0">'.$chart->pack.'</td>';
+    echo (in_array(4, $chart->sc)) ? $checked_cell[0].'bg-success'.$checked_cell[1] : $blank_cell;
+    echo (in_array(5, $chart->sc)) ? $checked_cell[0].'bg-primary'.$checked_cell[1] : $blank_cell;
+    echo (in_array(6, $chart->sc)) ? $checked_cell[0].'bg-warning'.$checked_cell[1] : $blank_cell;
+    echo (in_array(8, $chart->sc)) ? $checked_cell[0].'bg-danger'.$checked_cell[1] : $blank_cell;
+    echo (isset($chart->remarks) && !empty($chart->remarks)) ? '<td class="border-0 text-muted fw-light fst-italic">'.$chart->remarks.'</td>' : $blank_cell;
     echo '</tr>';
 }
