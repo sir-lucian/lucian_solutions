4523a8e135f641c6db2ab5e32873775a63abee03
Karn Kamolnavin <596976@egat.co.th>
2024-05-13 16:31:57 +0700

    Adjust DJMax Table

--- php/list.djmax.php
+++ php/list.djmax.php
@@ -4,15 +4,16 @@ $url = 'https://raw.githubusercontent.com/lucidkarn/lucian_solutions/main/json/l
 $json = file_get_contents($url);
 $data = json_decode($json);
 $blank_cell = '<td class="text-success text-center"></td>';
-$checked_cell = '<td class="text-success text-center">&check;</td>';
+$checked_cell = ['<td class="', ' text-center">&check;</td>'];
 
 foreach ($data->charts as $chart) {
     echo '<tr><td>'.$chart->name;
     echo (isset($chart->local_name) && !empty($chart->local_name)) ? ' <small lang="'.$chart->local_lang.'">'.$chart->local_name.'</small></td>' : '</td>';
-    echo (in_array(4, $chart->sc)) ? $checked_cell : $blank_cell;
-    echo (in_array(5, $chart->sc)) ? $checked_cell : $blank_cell;
-    echo (in_array(6, $chart->sc)) ? $checked_cell : $blank_cell;
-    echo (in_array(8, $chart->sc)) ? $checked_cell : $blank_cell;
+    echo '<td>'.$chart->pack.'</td>';
+    echo (in_array(4, $chart->sc)) ? $checked_cell[0].'text-success'.$checked_cell[1] : $blank_cell;
+    echo (in_array(5, $chart->sc)) ? $checked_cell[0].'text-primary'.$checked_cell[1] : $blank_cell;
+    echo (in_array(6, $chart->sc)) ? $checked_cell[0].'text-warning'.$checked_cell[1] : $blank_cell;
+    echo (in_array(8, $chart->sc)) ? $checked_cell[0].'text-danger'.$checked_cell[1] : $blank_cell;
     echo (isset($chart->remarks) && !empty($chart->remarks)) ? '<td>'.$chart->remarks.'</td>' : $blank_cell;
     echo '</tr>';
 }
